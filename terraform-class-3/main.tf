8c311ce72a47a66c175c9c843090214ba9528462
mahendra palla <mahendra33310@gmail.com>
2023-09-20 21:39:58 +0530

    variables1

--- terraform-class-3/main.tf
+++ terraform-class-3/main.tf
@@ -11,3 +11,9 @@ resource "aws_instance" "webserver" {
         Name = "webserver"
     } 
 }
+
+# OUTPUT  FILE CAN BE IN VARIABLE.TF FILE OR CAN BE IN MAIN.TF FILE 
+#output "public_ip" {
+#    description = "provide public IP of ec2 instance"
+#    value = aws_instance.webserver.public_ip
+#}
