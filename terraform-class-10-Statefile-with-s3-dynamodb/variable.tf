5499db7ee8491f95b574349d8d81485b61b11c25
mahendra palla <mahendra33310@gmail.com>
2023-12-10 20:07:13 +0530

    tfstate-lock-file

---
+++ terraform-class-10-Statefile-with-s3-dynamodb/variable.tf
@@ -0,0 +1,15 @@
+variable "region" {
+    description = "Provide the variable region"
+    default = ""  
+}
+
+variable "ami" {
+    description = "Provide the AMI ID"
+    default = ""
+}
+
+variable "instance_type" {
+    description = "Provide the Instance type"
+    default = ""
+}
+
