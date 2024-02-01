a20709dbb9767960f1e55cb79601aeee4fce0f1e
mahendra palla <mahendra33310@gmail.com>
2024-02-01 23:25:05 +0530

    IMPORT_1

---
+++ terraform-class-14-IMPORT/main.tf
@@ -0,0 +1,9 @@
+resource "aws_vpc" "stackorigin-vpc" {
+
+
+}
+
+
+# with this command, i have abled to import the VPC into state filecheck "name"
+
+# COMMAND:  terraform import aws_vpc.stackorigin-vpc vpc-01be39a0b94fb2a8c
